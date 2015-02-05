3318cf51c9178fd87d676c4471db19acccb4f89d
Jonathan <lostinmyfro@gmail.com>
2015-02-05 13:17:58 -0800

    Changed the #include to correct path for data.h in objectdic.h

--- Motor Driver Code/CAN Testing/canFiles/motor_objdict.h
+++ Motor Driver Code/CAN Testing/canFiles/motor_objdict.h
@@ -4,7 +4,7 @@
 #ifndef MOTOR_OBJDICT_H
 #define MOTOR_OBJDICT_H
 
-#include "data.h"
+#include "../canFestival/include/data.h"
 
 /* Prototypes of function provided by object dictionnary */
 UNS32 Motor_Board_valueRangeTest (UNS8 typeValue, void * value);
